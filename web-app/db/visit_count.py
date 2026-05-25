3ad3d755dff13c6ff8569dae6d263626ca4715ba
OriSason <orisason1@gmail.com>
2026-05-25 12:02:52 +0300

    cleaned code in /web-app/db/visit_count.py/increment_visit

--- web-app/db/visit_count.py
+++ web-app/db/visit_count.py
@@ -13,12 +13,11 @@ def increment_visit(page_path):
 
             if row:
                 query = sql.SQL("UPDATE visits SET count = count + 1 WHERE path = {page_path}").format(page_path=sql.Literal(page_path))
-                cursor.execute(query)
-                conn.commit()
             else:
                 query = sql.SQL("INSERT INTO visits (path, count) VALUES ({page_path}, 1)").format(page_path=sql.Literal(page_path))
-                cursor.execute(query)
-                conn.commit()
+            
+            cursor.execute(query)
+            conn.commit()
 
     except psycopg2.Error as e:
         print(f"Database error on increment_visit:\n")
