192b9084bb2891e6c7a94b3e80035cfe94463c77
OriSason <orisason1@gmail.com>
2026-07-22 18:58:34 +0300

    fixed RDS private RT association

--- terraform/rds/subnet.tf
+++ terraform/rds/subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "rds_priv_1" {
   cidr_block        = "172.20.4.0/24"
   availability_zone = var.zone1
   tags = {
-    Name = "devops-experts-rds-priv-sub"
+    Name = "devops-experts-rds-priv-sub-1"
   }
 }
 
@@ -11,8 +11,13 @@ resource "aws_route_table" "rds_priv_1_rt" {
   vpc_id = var.devops_experts_vpc_id
 
   tags = {
-    Name = "devops-experts-rds-priv-rt"
+    Name = "devops-experts-rds-priv-rt-1"
   }
+}
+
+resource "aws_route_table_association" "devops_experts_rds_priv_sub_1" {
+  subnet_id      = aws_subnet.rds_priv_1.id
+  route_table_id = aws_route_table.rds_priv_1_rt.id
 }
 
 resource "aws_subnet" "rds_priv_2" {
@@ -20,7 +25,7 @@ resource "aws_subnet" "rds_priv_2" {
   cidr_block        = "172.20.5.0/24"
   availability_zone = var.zone2
   tags = {
-    Name = "devops-experts-rds-priv-sub"
+    Name = "devops-experts-rds-priv-sub-2"
   }
 }
 
@@ -28,8 +33,13 @@ resource "aws_route_table" "rds_priv_2_rt" {
   vpc_id = var.devops_experts_vpc_id
 
   tags = {
-    Name = "devops-experts-rds-priv-rt"
+    Name = "devops-experts-rds-priv-rt-2"
   }
+}
+
+resource "aws_route_table_association" "devops_experts_rds_priv_sub_2" {
+  subnet_id      = aws_subnet.rds_priv_2.id
+  route_table_id = aws_route_table.rds_priv_2_rt.id
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
