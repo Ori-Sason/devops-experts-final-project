896dbaea49bd7cec21443fec45c3f5f9bac4407d
OriSason <orisason1@gmail.com>
2026-08-03 13:33:13 +0300

    added aws_autoscaling_policy

--- terraform/k3_cluster/web_app_asg.tf
+++ terraform/k3_cluster/web_app_asg.tf
@@ -49,3 +49,19 @@ resource "aws_autoscaling_group" "web_app_worker_asg" {
     aws_route_table_association.devops_experts_k3s_priv_sub
   ]
 }
+
+resource "aws_autoscaling_policy" "web_app_cpu_policy" {
+  name                      = "devops-experts-web-app-cpu-target-tracking"
+  autoscaling_group_name    = aws_autoscaling_group.web_app_worker_asg.name
+  policy_type               = "TargetTrackingScaling"
+  estimated_instance_warmup = 180
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 70.0 # Target 70% CPU usage across the ASG
+    disable_scale_in = true # Didn't add support in K3s cluster, yet
+  }
+}
